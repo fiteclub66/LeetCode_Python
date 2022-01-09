6580e2a8364f8b6c07134458a44dd5cdb2b35bcb
fiteclub66 <cfite11@gmail.com>
2022-01-09 16:24:19 -0500

    Add Two Numbers Complete

---
+++ Add_Two_Numbers.py
@@ -0,0 +1,39 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        first_num_string = ""
+        second_num_string = ""
+
+        current_node = l1
+        while current_node is not None:
+            first_num_string = str(current_node.val) + first_num_string
+            current_node = current_node.next
+
+        current_node = l2
+        while current_node is not None:
+            second_num_string = str(current_node.val) + second_num_string
+            current_node = current_node.next
+
+        total_num_string = str(int(first_num_string) + int(second_num_string))
+
+        head = None
+        previous_node = None
+        for i in range(len(total_num_string)):
+            node = ListNode(int(total_num_string[i]), None)
+            if previous_node is not None:
+                node.next = previous_node
+            previous_node = node
+            if (i + 1) == len(total_num_string):
+                head = node
+
+        return head
